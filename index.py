0002f38edb843897465ef708d823166235d70b45
T-REX-FAN <raon1588a@gmail.com>
2024-10-24 21:57:34 +0900

    변수를배웠어

--- index.py
+++ index.py
@@ -1,8 +1,16 @@
-print("안녕")
+# print("안녕")
 print("t-rex is strongest dinosaurus...not now")
 print("what is pow")
 
 
+# 변수
+# 데이터를 저장하는 톱
+
+# 첫글자가 영어나 음지점수여야 한다(첫글자는 숫자가 올수없다)
+# 푸가_예약어는 변수로 사용할수없다 
+# 푸가-대소문자가 구분된다 (T-rex =X t-rex)
+# 푸가_기계어에서는 t-rex 랑 T-rex 가 0110110000 이랑 1110001000100 이런 차이다
+# 푸가_안녕->아스키 코드->001110011010110
 
 
 
@@ -12,7 +20,27 @@ print("what is pow")
 
 
 
-
-
-
-
+# 자료형-자료아우 = 데이터의 종류
+# 숫,문 자 / 논리 / 벌집
+# 문자 =X 문자열 문자열 in 파이썬=스트링 스트링은 str까지만 사용 숫자는 두가지로 나눈다 -숫자 암자- 정수와 실수
+# 1 2 3 4 5 5.3 5/1 boolean bool ball
+# maromaro
+# 파이선은 자료형을 추론해준다--마녀사냥-- 변수를 선언하면 숫자를 넣든 군집을 넣든 뭐를 넣어도 다된다,변수는 자종차별자가 아닌듯
+maro = 5
+oram = 2
+# print(maro + oram)
+# print(maro - oram)
+# print(maro * oram)
+# print(maro / oram)
+# print(maro // oram)
+# print(maro % oram)
+# print(maro ** oram)
+# 표기할수있는 최대 숫자값이 30이라고쳤을때 31이되면은 다시 처음 숫자인 0 으로 돌아가는
+a = 4
+b  = 1.5
+c = True
+d = "me size 169.9"
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
